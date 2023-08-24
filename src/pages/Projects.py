6d41f13d0adbb34e8be9b920a3a698cf6415de45
Sahil Pattni <sahilpattni97@gmail.com>
2023-08-24 16:21:58 +0400

    Added rank attribute to YAML files for sorting

--- src/pages/Projects.py
+++ src/pages/Projects.py
@@ -1,15 +1,31 @@
-import streamlit as st
 from components import header, project_card
-import os
+import math
 import yaml
+import os
 
 header("Projects")
 
+# Parse project details from YAML files
+projects = []
 for filename in os.listdir("resources/projects"):
     if filename.endswith(".yaml"):
         with open(f"resources/projects/{filename}") as f:
             details = yaml.load(f, Loader=yaml.FullLoader)
-            url = details["url"] if "url" in details else None
-            project_card(
-                details["title"], details["tools"], details["description"], url
-            )
+            if "url" not in details:
+                details["url"] = None
+            if "rank" not in details:
+                details["rank"] = math.inf
+            projects.append(details)
+
+
+# Sort projects by rank (ascending)
+projects.sort(key=lambda x: x["rank"])
+
+# Generate project cards
+for project in projects:
+    project_card(
+        project["title"],
+        project["tools"],
+        project["description"],
+        project["url"],
+    )
