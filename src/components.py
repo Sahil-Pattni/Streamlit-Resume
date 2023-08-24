c44fcc24e622d6308f551cd3b1623b31446b4936
Sahil Pattni <sahilpattni97@gmail.com>
2023-08-24 16:22:18 +0400

    Added education component

--- src/components.py
+++ src/components.py
@@ -51,3 +51,35 @@ def project_card(title: str, tools: str, description: str, url: str = None) -> N
     st.markdown(description)
 
     st.divider()
+
+
+def education_card(
+    institution: str, program: str, duration: str, location: str, description: str
+) -> None:
+    """
+    Generates an education card.
+
+    Args:
+        institution (str): The name of the institution.
+        program (str): The program of study.
+        duration (str): The duration of the program.
+        description (str): The description of the program. Accepts markdown syntax.
+    """
+    st.markdown(f"### {institution}")
+    st.markdown(f"##### {program}")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown(
+            f'###### <div style="text-align: left;">{location}</div>',
+            unsafe_allow_html=True,
+        )
+    with col2:
+        st.markdown(
+            f'###### <div style="text-align: right;">{duration}</div>',
+            unsafe_allow_html=True,
+        )
+
+    st.markdown(description)
+
+    st.divider()
---
+++ src/pages/Education.py
@@ -0,0 +1,30 @@
+from components import header, education_card
+import math
+import yaml
+import os
+
+
+header("Education")
+
+# Parse education details from YAML files
+universities = []
+for filename in os.listdir("resources/education"):
+    if filename.endswith("yaml"):
+        with open(f"resources/education/{filename}") as f:
+            details = yaml.load(f, Loader=yaml.FullLoader)
+            if "rank" not in details:
+                details["rank"] = math.inf
+            universities.append(details)
+
+# Sort universities by rank (ascending)
+universities.sort(key=lambda x: x["rank"])
+
+# Generate education cards
+for university in universities:
+    education_card(
+        university["institution"],
+        university["program"],
+        university["duration"],
+        university["location"],
+        university["description"],
+    )
